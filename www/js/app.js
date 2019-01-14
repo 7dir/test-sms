df2021dad745eb34ff03aedb9bba583051bc4dd6
Mike <mltefive@gmail.com>
2019-01-14 09:42:03 +0900

    phone

--- www/js/app.js
+++ www/js/app.js
@@ -38,8 +38,8 @@ var app = {
         var options = {
             replaceLineBreaks: true, // true to replace \n by a new line, false by default
             android: {
-                // intent: 'INTENT'  // send SMS with the native android SMS messaging
-                intent: '' // send SMS without opening any other app
+                intent: 'INTENT'  // send SMS with the native android SMS messaging
+                // intent: '' // send SMS without opening any other app
             }
         };
 
